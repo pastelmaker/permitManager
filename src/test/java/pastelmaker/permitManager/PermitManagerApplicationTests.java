cbf7cb449deea4e90dcc833a2b2673e0d2817b05
Fabio Dassan <fabiodassan@gmail.com>
2016-08-09 17:07:43 -0300

    Add bootstrapped spring boot application

---
+++ src/test/java/pastelmaker/permitManager/PermitManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package pastelmaker.permitManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PermitManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
