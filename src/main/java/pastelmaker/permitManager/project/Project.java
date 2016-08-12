b64ec42bf1cfe92251a1e62927ed9599723cabfd
Fabio Dassan <fabiodassan@gmail.com>
2016-08-12 13:15:27 -0300

    Project representation

---
+++ src/main/java/pastelmaker/permitManager/project/Project.java
@@ -0,0 +1,54 @@
+package pastelmaker.permitManager.project;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import pastelmaker.permitManager.user.User;
+
+@Entity
+@Table(name = "Project")
+public class Project {
+
+    private long id;
+	private String name;
+	private String description;
+	private User leader;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getLeader() {
+		return leader;
+	}
+
+	public void setLeader(User leader) {
+		this.leader = leader;
+	}
+
+}
---
+++ src/main/java/pastelmaker/permitManager/project/ProjectRepository.java
@@ -0,0 +1,9 @@
+package pastelmaker.permitManager.project;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+}
